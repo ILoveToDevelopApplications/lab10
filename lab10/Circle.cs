e55af5214197360f1a8aa56f38c410b39d0fb6fc
ILoveToDevelopApplications <chezzplayzz@yahoo.com>
2018-07-30 09:11:57 -0400

    lab10

---
+++ lab10/Circle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Circle
+    {
+
+
+        public double radius;
+        public double area;
+        public double circumference;
+        public double pi;
+
+        private double Radius { get; set; }
+
+
+
+        public Circle(double radius)
+        {
+
+            Radius  = radius;
+            //pi = 3.14  ;
+           // area = pi * radius * radius;
+            //circumference = 2 * pi * radius;
+
+        }
+
+
+       
+        
+
+        public double CalculateCircumference()
+        {
+             var  circumference  = 2* Math.PI * Radius;
+
+            return circumference;
+        }
+
+
+        public string CalculateFormattedCircumference()
+        {
+            
+        }
+
+
+        public double CalculateArea()
+        {
+            var area = pi * radius * radius;
+
+            return area;
+        }
+
+
+        public string CalculateFormattedArea()
+        {
+            
+        }
+
+
+        private string FormatNumber(double )
+        {
+
+        }
+
+
+        /*  public string CalculateFormattedArea()
+          {
+
+          }
+
+
+          private string FormatNumber(double)
+          {
+
+          }
+           */
+
+
+
+
+
+
+    }
+
+}
---
+++ lab10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //private static double radius;
+
+            Console.WriteLine("\nWelcome to the Circle Tester");
+
+            bool toContinue = false;
+         
+           while(toContinue)
+          {
+
+             try
+                {
+             
+
+                    var cicumference = new Circle(radius);
+                    var area  = new Circle(radius);
+                    var areaResult = area.CalculateArea();
+                    var circumferenceResult = circumferenceResult.CalculatedFormatted();
+
+                }
+                catch(Exception e)
+                {
+
+
+                    Console.WriteLine(e.Messege);
+
+                }
+
+
+                Console.WriteLine("Contine y/n\t/");
+
+                var restart = char.ToLower(Console.ReadKey().KeyChar);
+
+                if (restart == 'y')
+                {
+                    toContinue = true;
+
+                }
+
+           }
+
+            
+  
+
+            Console.ReadKey();
+        }
+    }
+}
